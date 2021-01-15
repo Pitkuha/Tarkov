10ff5febbb1e0828bccf714e0a449dbf68271dee
Daniil Demichev <daniil_demichev@mail.ru>
2021-01-15 11:10:37 +0300

    person.html

--- src/main/java/app/repository/FighterInventoryRepository.java
+++ src/main/java/app/repository/FighterInventoryRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface FighterInventoryRepository extends JpaRepository<FighterInventory, FighterInventoryPK> {
+public interface FighterInventoryRepository extends JpaRepository<FighterInventory, Long> {
     List<FighterInventory> getAllByFighterId(Fighter byId);
 }
--- src/main/java/app/service/TaskDTOService.java
+++ src/main/java/app/service/TaskDTOService.java
@@ -1,10 +1,9 @@
 package app.service;
 
-import app.domain.Task;
-import app.domain.TraderInventory;
-import app.domain.TraderTasks;
+import app.domain.*;
 import app.repository.*;
 import app.util.Tasks;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,12 +16,29 @@ public class TaskDTOService {
     private final TraderTasksRepository traderTasksRepository;
     private final TraderInventoryRepository traderInventoryRepository;
     private final TraderRepository traderRepository;
+    private final FighterInventoryRepository fighterInventoryRepository;
+    private final MedicineRepository medicineRepository;
+    private final AmmunitionRepository ammunitionRepository;
+    private final HelmetRepository helmetRepository;
+    private final WeaponRepository weaponRepository;
+    private final ProvisionRepository provisionRepository;
+    private final ArmorRepository armorRepository;
+    private final MagazinRepository magazinRepository;
 
-    public TaskDTOService(TaskRepository taskRepository, TrustRepository trustRepository, FighterRepository fighterRepository, TraderRepository traderRepository, TraderTasksRepository traderTasksRepository, TraderInventoryRepository traderInventoryRepository, TraderRepository traderRepository1) {
+    public TaskDTOService(TaskRepository taskRepository, TrustRepository trustRepository, FighterRepository fighterRepository, TraderRepository traderRepository, TraderTasksRepository traderTasksRepository, TraderInventoryRepository traderInventoryRepository, TraderRepository traderRepository1, FighterInventoryRepository fighterInventoryRepository, MedicineRepository medicineRepository, AmmunitionRepository ammunitionRepository, HelmetRepository helmetRepository, WeaponRepository weaponRepository, ProvisionRepository provisionRepository, ArmorRepository armorRepository, MagazinRepository magazinRepository) {
         this.fighterRepository = fighterRepository;
         this.traderTasksRepository = traderTasksRepository;
         this.traderInventoryRepository = traderInventoryRepository;
         this.traderRepository = traderRepository1;
+        this.fighterInventoryRepository = fighterInventoryRepository;
+        this.medicineRepository = medicineRepository;
+        this.ammunitionRepository = ammunitionRepository;
+        this.helmetRepository = helmetRepository;
+        this.weaponRepository = weaponRepository;
+
+        this.provisionRepository = provisionRepository;
+        this.armorRepository = armorRepository;
+        this.magazinRepository = magazinRepository;
     }
 
     public List<Tasks> getAllAvailableTasks(String name){
@@ -39,6 +55,23 @@ public class TaskDTOService {
 //        traderTasksRepository.save(new TraderTasks(traderRepository.getOne(23L),taskRepository.getOne(26L)));
 //        traderTasksRepository.save(new TraderTasks(traderRepository.getOne(24L),taskRepository.getOne(26L)));
 //        traderTasksRepository.save(new TraderTasks(traderRepository.getOne(24L),taskRepository.getOne(27L)));
+//          fighterInventoryRepository.save(new FighterInventory(fighterRepository.getOne(22L), null, ammunitionRepository.getOne(75L), null, null,null,null,null,1));
+//          fighterInventoryRepository.save(new FighterInventory(fighterRepository.getOne(22L), null, null, null, null,null,null, helmetRepository.getOne(76L), 1));
+//          fighterInventoryRepository.save(new FighterInventory(fighterRepository.getOne(22L), weaponRepository.getOne(77L), null, null, null,null,null,null,2));
+//          fighterInventoryRepository.save(new FighterInventory(fighterRepository.getOne(22L), null, null, null, null, provisionRepository.getOne(78L), null,null,12));
+//          fighterInventoryRepository.save(new FighterInventory(fighterRepository.getOne(22L), null, null, null, null,null, armorRepository.getOne(79L), null,2));
+//          fighterInventoryRepository.save(new FighterInventory(fighterRepository.getOne(22L), null, null, magazinRepository.getOne(80L), null,null,null,null,20));
+//          traderInventoryRepository.save(new TraderInventory(traderRepository.getOne(23L), null, null, null, medicineRepository.getOne(61L),null,null,null,2, 1000));
+//          traderInventoryRepository.save(new TraderInventory(traderRepository.getOne(24L), null, null, null, medicineRepository.getOne(61L),null,null,null,3, 1500));
+//        medicineRepository.save(new Medicine("Аптечка", 100));
+//        medicineRepository.save(new Medicine("Пластырь", 20));
+//        fighterInventoryRepository.deleteById(71L);
+//        ammunitionRepository.save(new Ammunition(3,4));
+//        helmetRepository.save(new Helmet("Шлем", 1, "Сталь", 50, 50));
+//        weaponRepository.save(new Weapon("Пистолет", "Скиппи", 80, 5, 5, 5, 100));
+//        provisionRepository.save(new Provision("Снэк", 50));
+//        armorRepository.save(new Armor("Броник", 3, "Сталь", 5));
+//        magazinRepository.save(new Magazin(2,2));
         List<Tasks> fromDB = traderTasksRepository.FindAllWithDescriptionQuery(name);
         return fromDB;
     }
