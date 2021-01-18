33edb7c62ace52a8df7a4576b6d0580cf9c73693
Daniil Demichev <daniil_demichev@mail.ru>
2021-01-18 16:54:10 +0300

    Фоточки вот они

--- src/main/java/app/config/DataSourceConfig.java
+++ src/main/java/app/config/DataSourceConfig.java
@@ -18,7 +18,7 @@ public class DataSourceConfig {
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
         dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
         dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("fooflo");
+        dataSourceBuilder.password("Postgreshehmda");
 
         return dataSourceBuilder.build();
     }
